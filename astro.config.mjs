e1d40f208a3f01872a2cda4201bc64f391ea3c02
YK <1811651+ykdojo@users.noreply.github.com>
2026-02-24 02:09:09 -0800

    Remove base path - let monorepo rewrites handle routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.eventual.ai',
-  base: '/blog',
   integrations: [mdx(), sitemap()],
 });
